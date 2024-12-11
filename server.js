0e3f492272d9251e6154825584330f71f01e9280
aaskar rana <aaskar04rana@gmail.com>
2024-12-11 22:52:36 +0530

    database connected

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
+const { connect } = require("mongoose");
+const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 
 const port =process.env.PORT || 5000;
 
+connectDb();
 const app = express();
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes")) 
